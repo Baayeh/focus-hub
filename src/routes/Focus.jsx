2658f7a0cfa3e197869b1458154273a4eacbeeab
I am Alaska <stephen.alaska94@hotmail.com>
2022-11-06 17:10:26 +0000

    design focus session layout

--- src/routes/Focus.jsx
+++ src/routes/Focus.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Focus = () => {
   return (
-    <section id='focus'>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat animi earum ea dolor doloremque facere! Eos tenetur accusantium, consequatur libero sint unde voluptate dolorum neque dicta fuga corporis odit praesentium tempore! Nihil ut necessitatibus beatae et blanditiis asperiores nesciunt commodi in dolor sequi ratione incidunt, provident quidem porro sed recusandae consequuntur soluta. Assumenda quod consequuntur quas odio recusandae velit quaerat cum maiores, magni voluptatum eligendi aliquid ullam architecto id minus a, modi tempora odit numquam. Sequi, eaque laboriosam! Architecto quo voluptatem hic nisi consequatur aliquam quia minus maxime aperiam. Nisi perferendis numquam consectetur odit reprehenderit assumenda mollitia necessitatibus ut sit! Adipisci reiciendis doloribus, architecto repudiandae veritatis voluptate aut laudantium ut odio vero recusandae voluptatum nulla rerum nam autem velit, impedit aliquid, voluptates optio officia delectus repellendus? Accusantium, sequi sapiente commodi, quo soluta pariatur eius blanditiis praesentium, quas dolores earum at libero. Debitis cupiditate natus tempora, autem amet sint. Perspiciatis tenetur temporibus a distinctio delectus quia est. Doloribus amet officia eligendi autem asperiores ab nihil, vitae, commodi optio quos eum vero odio perspiciatis dolor ut iure architecto, ea assumenda suscipit doloremque! Ab, quaerat ullam ipsum, modi aliquid incidunt amet, provident debitis omnis accusamus ducimus rem minima quisquam autem. Qui, a accusantium?
+    <section
+      id="focus"
+      className="py-4 md:py-10 px-5  grid grid-cols-1 md:grid-cols-2 gap-x-5"
+    >
+      <div className="focus-todo-section">
+        <div className="card bg-white shadow-white-shadow rounded-lg mb-3">
+          <div className="card-body p-3">Focus Section</div>
+        </div>
+        <div className="card bg-white shadow-white-shadow rounded-lg mb-3">
+          <div className="card-body p-3">Todo</div>
+        </div>
+      </div>
+      <div className="progress-entertainment">
+        <div className="card bg-white shadow-white-shadow rounded-lg mb-3">
+          <div className="card-body p-3">Progress</div>
+        </div>
+        <div className="card bg-white shadow-white-shadow rounded-lg mb-3">
+          <div className="card-body p-3">Spotify</div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Focus
+export default Focus;
