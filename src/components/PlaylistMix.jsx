4c3db9a02bee7e35605a14545ca960d4d2787075
I am Alaska <stephen.alaska94@hotmail.com>
2022-11-07 22:26:11 +0000

    create all playlist section with framer animation

--- src/components/PlaylistMix.jsx
+++ src/components/PlaylistMix.jsx
@@ -1,12 +1,21 @@
+import { BsFillPlayFill } from "react-icons/bs";
+
 const PlaylistMix = () => {
   return (
-    <div className="playlist-mix p-2 bg-gray-100 rounded-lg cursor-pointer">
+    <div className="playlist-mix p-2 bg-gray-100 rounded-lg cursor-pointer hover:bg-gray-200">
       <div
         className="album-cover relative w-full min-h-[120px] bg-cover bg-center"
         style={{
           backgroundImage: `url(https://images.unsplash.com/photo-1504898770365-14faca6a7320?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1967&q=80)`,
         }}
-      ></div>
+      >
+        <button
+          type="button"
+          className="bg-primaryColor w-[35px] h-[35px] rounded-full absolute bottom-0 right-3 z-10 flex opacity-0 items-center justify-center shadow-md play-btn"
+        >
+          <BsFillPlayFill className="text-white text-xl"></BsFillPlayFill>
+        </button>
+      </div>
       <div className="px-1 py-2">
         <h6 className="font-black text-sm leading-none">2000s Mix</h6>
         <span className="text-[10px] mt-1 block">
--- src/components/musicPlaylist.jsx
+++ src/components/musicPlaylist.jsx
@@ -1,20 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TbDots } from "react-icons/tb";
-import { BsSpotify } from "react-icons/bs";
-import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
-import PlaylistMix from "../components/PlaylistMix";
+import { BsSpotify, BsArrowLeftShort } from "react-icons/bs";
+import MusicOverview from "../components/musicOverview";
+import AllMusicPlaylist from "../components/allMusicPlalist";
 
 const MusicPlaylist = () => {
-  const [recentSwiper, setRecentSwiperInstance] = useState(null);
-  const [focusSwiper, setFocusSwiperInstance] = useState(null);
+  const [pageState, setPageState] = useState("overview");
+  const [playlistType, setPlaylistType] = useState(null);
 
-  console.log(window.innerWidth);
+  const viewAllPlaylist = (playlistType, page) => {
+    setPlaylistType(playlistType);
+    setPageState(page);
+  };
+
+  const renderMusicPage = () => {
+    switch (pageState) {
+      case "overview":
+        return (
+          <MusicOverview viewAllPlaylist={viewAllPlaylist}></MusicOverview>
+        );
+      case "allPage":
+        return (
+          <AllMusicPlaylist playlistType={playlistType}></AllMusicPlaylist>
+        );
+
+      // default:
+      //   return (
+      //     <MusicOverview viewAllPlaylist={viewAllPlaylist}></MusicOverview>
+      //   );
+    }
+  };
+
+  useEffect(() => {
+    renderMusicPage();
+  }, [pageState]);
 
   return (
-    <section id="music-playlist">
+    <section id="music-playlist" className="overflow-hidden">
       <div className="playlist-header flex items-center justify-between">
         <div className="flex items-center">
+          {pageState !== "overview" ? (
+            <button
+              type="button"
+              className="mr-2"
+              onClick={() => setPageState("overview")}
+            >
+              <BsArrowLeftShort className="text-2xl"></BsArrowLeftShort>
+            </button>
+          ) : null}
+
           <BsSpotify className="mr-2 text-xl"></BsSpotify>
           <h6 className="text-sm font-bold">Spotify</h6>
         </div>
@@ -24,101 +58,7 @@ const MusicPlaylist = () => {
         </button>
       </div>
 
-      <div className="playlist-content mt-3 px-3">
-        <div className="recent-played mb-4">
-          <div className="items-center flex justify-between">
-            <span className="font-bold block text-sm">Recently Played</span>
-
-            <a href="" className="text-xs">
-              See all
-            </a>
-          </div>
-
-          <div className="music-list relative mt-2">
-            <button
-              type="button"
-              className="swiper-nav-btn left"
-              onClick={() => recentSwiper.slidePrev()}
-            >
-              <FaAngleLeft></FaAngleLeft>
-            </button>
-            <button
-              type="button"
-              className="swiper-nav-btn right"
-              onClick={() => recentSwiper.slideNext()}
-            >
-              <FaAngleRight></FaAngleRight>
-            </button>
-            <Swiper
-              spaceBetween={10}
-              slidesPerView={window.innerWidth > 1200 ? 3 : 2}
-              onSlideChange={() => console.log("slide change")}
-              onSwiper={(swiper) => setRecentSwiperInstance(swiper)}
-              ref={recentSwiper}
-            >
-              <SwiperSlide>
-                <PlaylistMix></PlaylistMix>
-              </SwiperSlide>
-              <SwiperSlide>
-                <PlaylistMix></PlaylistMix>
-              </SwiperSlide>
-              <SwiperSlide>
-                <PlaylistMix></PlaylistMix>
-              </SwiperSlide>
-              <SwiperSlide>
-                <PlaylistMix></PlaylistMix>
-              </SwiperSlide>
-            </Swiper>
-          </div>
-        </div>
-
-        <div className="recent-played">
-          <div className="items-center flex justify-between">
-            <span className="font-bold block text-sm">Focus</span>
-
-            <a href="" className="text-xs">
-              See all
-            </a>
-          </div>
-
-          <div className="music-list relative mt-2">
-            <button
-              type="button"
-              className="swiper-nav-btn left"
-              onClick={() => focusSwiper.slidePrev()}
-            >
-              <FaAngleLeft></FaAngleLeft>
-            </button>
-            <button
-              type="button"
-              className="swiper-nav-btn right"
-              onClick={() => focusSwiper.slideNext()}
-            >
-              <FaAngleRight></FaAngleRight>
-            </button>
-            <Swiper
-              spaceBetween={10}
-              slidesPerView={window.innerWidth > 1200 ? 3 : 2}
-              onSlideChange={() => console.log("slide change")}
-              onSwiper={(swiper) => setFocusSwiperInstance(swiper)}
-              ref={focusSwiper}
-            >
-              <SwiperSlide>
-                <PlaylistMix></PlaylistMix>
-              </SwiperSlide>
-              <SwiperSlide>
-                <PlaylistMix></PlaylistMix>
-              </SwiperSlide>
-              <SwiperSlide>
-                <PlaylistMix></PlaylistMix>
-              </SwiperSlide>
-              <SwiperSlide>
-                <PlaylistMix></PlaylistMix>
-              </SwiperSlide>
-            </Swiper>
-          </div>
-        </div>
-      </div>
+      {renderMusicPage()}
     </section>
   );
 };
